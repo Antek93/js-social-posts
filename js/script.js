2a91116d03366f4585c8f5d8369d13288b0f94ff
Antek93 <antonio.savino1993@gmail.com>
2023-01-18 16:25:43 +0100

    inizializzato il file

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-# js-social-posts
+console.log('HI, I am java and I am working fine');
 
-Ciao ragazzi,
 
-esercizio di oggi: Social Posts
-nome repo: js-social-posts
-
-Descrizione
 Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+
 Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post.
 
 Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
@@ -30,12 +26,8 @@ Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 
 BONUS
 
-1. Formattare le date in formato italiano (gg/mm/aaaa)
-2. Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola LF).
-3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
-Consigli del giorno:
-Ragioniamo come sempre a step.
-Prima scriviamo nei commenti la logica in italiano e poi traduciamo in codice.
-console.log() è nostro amico.
-Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
-Buon lavoro! (modificato) 
+1. Formattare le date in formato italiano (gg/mm/aaaa);
+
+2. Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola LF);
+
+3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone;
