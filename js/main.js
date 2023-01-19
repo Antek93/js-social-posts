dcb1fd0b0c999fd24445ff67e896516837dec164
Antek93 <antonio.savino1993@gmail.com>
2023-01-19 15:46:42 +0100

    aggiunte note

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log('HI, I am java and I am working fine');
+console.log('HI, I am java and I am working fine');  // ↑ ↓ ← →
 
 // Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 
@@ -102,7 +102,6 @@ let container = document.getElementById('container');
 
 for (let index = 0; index < posts.length; index++) {
     
-
     let newDiv = document.createElement('div');
     newDiv.innerHTML = `
     <div class="post">
@@ -134,57 +133,72 @@ for (let index = 0; index < posts.length; index++) {
                     </div>
                 </div> 
             </div>            
-        </div>`
-
+        </div>
+        `
         container.append(newDiv);
-
-    
 }
 
 // Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 
+
+// Selezionate la classe js-like-button per identificare il container del like button
+
 const allLikeButtons = document.querySelectorAll('.js-like-button');
+
+// Creato archivio per contenere tutti 
 
 const likedPosts = [];
 
+// <---------------------CREATO CICLO PER RIPETERE AZIONE CLICK SU OGNI LIKE BUTTON ------------------------>
+
+
 allLikeButtons.forEach((singleButton, i) => {
 
+// <---------------------------------------------AZIONE CLICK ----------------------------------------------->
 
     singleButton.addEventListener('click',
     
-    function(event) {
+    function(event) { //EVENT riferito all'azione del cliccare
 
-        event.preventDefault();
+        event.preventDefault(); // aka = quando clicco previeni evento di default
+        // (in questo caso, il ricaricare la pagina a vuoto riga 126 href='#' azione di default di a)
 
-        if(!this.classList.contains('like-button--liked')) {
+        if(!this.classList.contains('like-button--liked')) { // SE (if) L'ELEMENTO CLICCATO (this) NON (!this) CONTIENE (.contains) la classe 'like-button--liked'
 
-                this.classList.add('like-button--liked');
+                this.classList.add('like-button--liked'); // AGGIUNGI (add) la classe 'like-button--liked' all'elemento cliccato
 
-                const postId = this.getAttribute('data-postid');
+                const postId = this.getAttribute('data-postid'); // postId = elemento cliccato con (this) con attributo 'data-postid'
+                //SELEZIONAMO QUESTO ELEMENTO PER IDENTIFICARE UNO DEI 5 DIV CREATI CON IL CICLO FOR EACH (riga 126)
 
-                const likesCounter = document.getElementById('like-counter-' + postId);
+                const likesCounter = document.getElementById('like-counter-' + postId); // 'like-counter-' + postId = like-counter-${posts[index]['id']} della riga 132
+                // ↑ creiamo variabile per associare l'id specifico di b (riga 132) con classe 'like-counter' 
 
-                // Incrementa il valore contenuto dentro likesCounter di 1
-
-                let likes = parseInt(likesCounter.innerText);
-                likes = likes + 1;
-                likesCounter.innerHTML = likes;
                 
+                let likes = parseInt(likesCounter.innerText);
+                //All'interno del likesCounter per ogni click sul contatore: likes incrementa di 1
+                likes = likes + 1;
+
+                likesCounter.innerHTML = likes; //Mostra all'interno del counter il numero di likes
+
+                //Pusha all'interno dell'archivio precedentemente creato (riga 150) l'id di <a> riga 126
                 likedPosts.push(postId);
 
-        } else {
+        } else { // ALTRIMENTI (else) se L'ELEMENTO CLICCATO (this) CONTIENE (.contains) la classe 'like-button--liked'
+        
+        const postId = this.getAttribute('data-postid'); //recupera il suo id pushato nell'array postId
 
+        likedPosts.splice(likedPosts.indexOf(postId), 1); //rimuovi dall'array l'id pushato
 
-        const postId = this.getAttribute('data-postid');
+        const likesCounter = document.getElementById('like-counter-' + postId) // 'like-counter-' + postId = like-counter-${posts[index]['id']} della riga 132
+        // ↑ creiamo variabile per associare l'id specifico di b (riga 132) con classe 'like-counter' 
 
-        likedPosts.splice(likedPosts.indexOf(postId), 1);
-
-        const likesCounter = document.getElementById('like-counter-' + postId)
-        let likes = parseInt(likesCounter.innerText);
+        let likes = parseInt(likesCounter.innerText); 
+        //All'interno del likesCounter per ogni click sul contatore: likes decrementa di 1
         likes = likes - 1;
-        likesCounter.innerHTML = likes;
+        likesCounter.innerHTML = likes; //Mostra all'interno del counter il numero di likes
 
         this.classList.remove('like-button--liked');
+        // RIMUOVI (remove) la classe 'like-button--liked' all'elemento cliccato
 
         }
     });
